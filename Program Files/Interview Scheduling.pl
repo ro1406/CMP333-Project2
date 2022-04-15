d44bef002bfc10483385cbf8ecbe772b8587a418
Rohan <rohanmitra8@gmail.com>
2022-04-15 16:04:39 +0400

    Interview Shed Done

--- Program Files/Interview Scheduling.pl
+++ Program Files/Interview Scheduling.pl
@@ -12,26 +12,25 @@
 
 scheduleInterviews([A,B,C,D,E]) :-
     People = [A,B,C,D,E],
-    People ins 1..6, all_different(People),
-    A #>= 2, A #=< 4, 			%Ali is busy before 2 and after 4
+    People ins 1..5, all_different(People),
+    A #>= 2, A #< 4, 			%Ali is busy before 2 and after 4
     B #\= 1,B #\= 3,B #\= 5,	%Ben does not like odd hours
     C #\= 2,C #\= 4,			%Cam has similar appointments at 2 and 4
     D #< 3,						%Don is free before 3
-    E #> 3,						%Eva only after 3
+    E #>= 3,					%Eva only after 3
     label(People).             
 	
 scheduleInterviews2([A,B,C,D,E]) :- %Additional Constraint : Don Follows right after Cam
     People = [A,B,C,D,E],
-    People ins 1..6, all_different(People),
-    A #>= 2, A #=< 4, 			%Ali is busy before 2 and after 4
+    People ins 1..5, all_different(People),
+    A #>= 2, A #< 4, 			%Ali is busy before 2 and after 4
     B #\= 1,B #\= 3,B #\= 5,	%Ben does not like odd hours
     C #\= 2,C #\= 4,			%Cam has similar appointments at 2 and 4
     D #< 3,						%Don is free before 3
-    E #> 3,						%Eva only after 3
+    E #>= 3,					%Eva only after 3
     D #= C+1,					%Don follows right after Cam
     label(People).
 
 
 % Your example queries go here
-?- scheduleInterviews(People).
 ?- scheduleInterviews2(People).
